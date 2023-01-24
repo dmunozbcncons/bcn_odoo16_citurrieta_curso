68d369df99646404d80fd5763653a783cebbac1f
bcn.cir <dmunoz@bcncons.com>
2023-01-24 18:54:21 +0000

    [add] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    'summary': """"Academy app to manage Trainig""",
+    'desciption':"""
+            Modulo de academia
+            -Cursos
+            -Sesiones
+            -Attendees
+    """,
+    'author':'Odoo',
+    'website': 'https://www.odoo.com',
+    'category':'Training',
+    'version':'0.1',
+    'depends': ['base'],
+    'data': [],
+    'demo':[]
+}
