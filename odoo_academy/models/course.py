8d623e7f5a002ce642db5a1d788a6e45f6e301e2
bcn.cir <dmunoz@bcncons.com>
2023-01-25 13:40:43 +0000

    [add] new fields

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -5,3 +5,8 @@ class Course(models.Model):
     _name = 'academy.course'
     _description = 'Informacion del curso'
     
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text(string='Descripción')
+    level = fields.Selection(string='Level',selection = [('beginer','Beginer'),('intermediate','Intermediate'),('advanced','Advanced')], copy=False)
+    activate = fields.Boolean(string='Activate',default=True)
+    
